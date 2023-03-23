886ba74d972a6212e318577469c918a51e69b851
vo khoa <vonguyenkhoa1998@gmail.com>
2023-03-23 11:11:45 +0700

    Buoi 3

---
+++ DTOs/FollowingDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VoNguyenKhoa_2080600032_BigSchool.DTOs
+{
+    public class FollowingDto
+    {
+        public string FolloweeId { get; set; }
+    }
+}
